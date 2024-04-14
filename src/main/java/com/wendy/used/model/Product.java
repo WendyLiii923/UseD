2fa3683923770fc78a09b7e8540d6765db67b84b
WendyLiii923 <107597824+WendyLiii923@users.noreply.github.com>
2024-04-14 21:13:31 +0800

    Update Product.java

--- src/main/java/com/wendy/used/model/Product.java
+++ src/main/java/com/wendy/used/model/Product.java
@@ -11,8 +11,11 @@ import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import lombok.Data;
+
+@Data
 @Entity
-@Table(name = "used_product")
+@Table(name = "product")
 public class Product {
 
 	@Id
@@ -21,7 +24,7 @@ public class Product {
 	private Integer id;
 	
 	@JsonFormat(pattern = "yyyy-MM-dd hh:mm")
-	@Column(name = "createTime")
+	@Column(name = "create_time")
 	private Timestamp createTime = new Timestamp(System.currentTimeMillis());
 	
 	@Column(name = "name")
@@ -33,10 +36,10 @@ public class Product {
 	@Column(name = "content")
 	private String content;
 
-	@Column(name = "memberId")
+	@Column(name = "member_id")
 	private Integer memberId;
 	
-	@Column(name = "categoryId")
+	@Column(name = "category_id")
 	private Integer categoryId;
 
 	@Column(name = "cover")
@@ -44,107 +47,5 @@ public class Product {
 
 	@Column(name = "status")
 	private String status;
-
-	public Product() {
-
-	}
-
-	public Product(Integer id, Timestamp createTime, String name, Integer price, String content, Integer memberId,
-			Integer categoryId, String cover, String status) {
-		super();
-		this.id = id;
-		this.createTime = createTime;
-		this.name = name;
-		this.price = price;
-		this.content = content;
-		this.memberId = memberId;
-		this.categoryId = categoryId;
-		this.cover = cover;
-		this.status = status;
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public Timestamp getCreateTime() {
-		return createTime;
-	}
-
-	public void setCreateTime(Timestamp createTime) {
-		this.createTime = createTime;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Integer getMemberId() {
-		return memberId;
-	}
-
-	public void setMemberId(Integer memberId) {
-		this.memberId = memberId;
-	}
-
-	public Integer getCategoryId() {
-		return categoryId;
-	}
-
-	public void setCategoryId(Integer categoryId) {
-		this.categoryId = categoryId;
-	}
-
-	public String getCover() {
-		return cover;
-	}
-
-	public void setCover(String cover) {
-		this.cover = cover;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
-
-	@Override
-	public String toString() {
-		return "UsedProduct [id=" + id + ", createTime=" + createTime + ", name=" + name + ", price=" + price
-				+ ", content=" + content + ", memberId=" + memberId + ", categoryId=" + categoryId + ", cover=" + cover
-				+ ", status=" + status + "]";
-	}
-
-	
-	
-
-
 
 }
